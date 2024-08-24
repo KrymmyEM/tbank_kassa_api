768a66aeb01925e8e9b8beb9f4a1529a32f2b9b5
Evgeny (Krymmy) Momotov <evgeny.momotov@gmail.com>
2024-08-24 11:52:12 +0300

    [UP] Init tb_mod > send_mod

--- tbank_kassa_api/tbank_models.py
+++ tbank_kassa_api/tbank_models.py
@@ -160,46 +160,6 @@ class ReceiptFFD12_2(ReceiptBase):
     Payments: Optional[List[Payments]]
 
 
-class Init(BaseModel):
-    OrderId: constr(max_length=36)
-    Amount: int
-    Description: Optional[constr(max_length=250)] = None
-    CustomerKey: Optional[constr(max_length=36)] = None
-    Ecurrent: Optional[constr(max_length=1)] = None
-    PayType: Optional[PayType] = None
-    Language: Optional[constr(max_length=2)] = None
-    NotificationURL: Optional[str] = None
-    SuccessURL: Optional[str] = None
-    FailURL: Optional[str] = None
-    RedirectDueDate: Optional[str] = None
-    DATA: Optional[dict] = None
-    Receipt: Union[ReceiptFFD105, ReceiptFFD12] = None
-    Shops: Optional[List[Shop]] = None
-
-    @root_validator(pre=True)
-    def check_fields(cls, values):
-        customer_key = values.get("CustomerKey")
-        recurrent = values.get("Ecurrent")
-
-        if recurrent and not customer_key:
-            raise ValueError("CustomerKey is required with Ecurrent")
-
-        amount = values.get("Amount")
-        receipt = values.get("Receipt")
-        
-        if receipt:
-            true_amount: int = 0
-            for item in receipt.Items:
-                true_amount += item.Amount
-
-            if true_amount != amount:
-                raise ValueError("Amount does not equal the sum of the Amount of items")
-
-        return values
-
-
-
-
 class NotificationBase(BaseModel):
     TerminalKey: constr(max_length=20) = Field(..., description="Идентификатор терминала. Выдается Мерчанту Т‑Кассой при заведении терминала")
     Success: bool = Field(..., description="Успешность прохождения запроса (true/false)")
